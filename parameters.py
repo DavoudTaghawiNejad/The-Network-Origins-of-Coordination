64d603d640dd7f7103bac6f9740d9fa1fee5f19a
davoud taghawi-nejad <davoud@taghawi-nejad.de>
2015-06-18 16:31:22 -0300

    multiple strategies OR multiple player numbers can be run

--- parameters.py
+++ parameters.py
@@ -4,12 +4,6 @@ from __future__ import division
 #number of time steps
 timeSteps=1000000
 
-#number of players
-numPlayers=100
-
-#number of strategies
-numStrategies=100
-
 #mean degree
 meanDegree=2
 
